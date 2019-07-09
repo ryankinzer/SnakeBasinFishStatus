fd5e689dafa4d7d70a6a97791ba0540f6bf269e3
Kevin See <Kevin.See@biomark.com>
2019-07-09 14:47:42 -0700

    added a line to read in trap database using function from STADEM (loadLGTrappingDBase function doesn't work for Macs).
    Also fixed the indents in the loop.

--- R/01_STADEM_model_run.R
+++ R/01_STADEM_model_run.R
@@ -16,6 +16,8 @@ library(STADEM)
 source('./R/loadLGTrappingDBase.R')
 trap_filepath <- './data/LGTrappingExportJodyW.accdb'
 trap_dbase <- loadLGTrappingDBase(trap_filepath)
+# uses STADEM function
+trap_dbase <- readLGRtrapDB(trap_filepath)
 
 # set species and spawn year
 species = c('Chinook', 'Steelhead')  # either Chinook or Steelhead
@@ -42,41 +44,41 @@ for(i in 1:length(species)){
       end_date = paste0(yr,'0630')
     }
     
-
-stadem_list = compileGRAdata(spp = spp,
-                             start_date = start_date,
-                             end_date = end_date,
-                             strata_beg = 'Mon',
-                             trap_dbase = trap_dbase,
-                             incl_jacks = incl_jacks)   
-
-# creates JAGs data list.
-jags_data_list = prepJAGS(stadem_list[['weeklyData']])
-
-# JAGs needs to access a .txt file of the model code
-model_file_nm = './ModelFiles/STADEM_LGR_model.txt'
-
-# what distribution to use for window counts?
-win_model = c('pois', 'neg_bin', 'neg_bin2', 'quasi_pois', 'log_space')[2]
-
-# run model - runSTADEMmodel sets the params to save inside the fnc, and it
-# does not save all the params available in the model!
-stadem_mod = runSTADEMmodel(file_name = model_file_nm,
-                            mcmc_chainLength = 40000,
-                            mcmc_burn = 10000,
-                            mcmc_thin = 30,
-                            mcmc_chains = 4,
-                            jags_data = jags_data_list,
-                            seed = 5,
-                            weekly_params = T,
-                            win_model = win_model)
-
-#ests <- stadem_mod$summary
-
-# save results
-save(stadem_mod, stadem_list,
-     file = paste0('./STADEM_results/LGR_STADEM_', spp, '_', yr, '.rda'))
-
+    
+    stadem_list = compileGRAdata(spp = spp,
+                                 start_date = start_date,
+                                 end_date = end_date,
+                                 strata_beg = 'Mon',
+                                 trap_dbase = trap_dbase,
+                                 incl_jacks = incl_jacks)   
+    
+    # creates JAGs data list.
+    jags_data_list = prepJAGS(stadem_list[['weeklyData']])
+    
+    # JAGs needs to access a .txt file of the model code
+    model_file_nm = './ModelFiles/STADEM_LGR_model.txt'
+    
+    # what distribution to use for window counts?
+    win_model = c('pois', 'neg_bin', 'neg_bin2', 'quasi_pois', 'log_space')[2]
+    
+    # run model - runSTADEMmodel sets the params to save inside the fnc, and it
+    # does not save all the params available in the model!
+    stadem_mod = runSTADEMmodel(file_name = model_file_nm,
+                                mcmc_chainLength = 40000,
+                                mcmc_burn = 10000,
+                                mcmc_thin = 30,
+                                mcmc_chains = 4,
+                                jags_data = jags_data_list,
+                                seed = 5,
+                                weekly_params = T,
+                                win_model = win_model)
+    
+    #ests <- stadem_mod$summary
+    
+    # save results
+    save(stadem_mod, stadem_list,
+         file = paste0('./STADEM_results/LGR_STADEM_', spp, '_', yr, '.rda'))
+    
   } # close j loop
 } # close i loop
 
