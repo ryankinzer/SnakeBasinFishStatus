1c4ee032680fbe8912aa61f1cdd3745ae830c766
Ryan Kinzer <ryank@nezperce.org>
2019-10-03 10:14:54 -0600

    changed year

--- R/03_ProcessCapHist_BulkYear.R
+++ R/03_ProcessCapHist_BulkYear.R
@@ -25,115 +25,13 @@ if(!dir.exists(DABOMdataFolder)) {
   dir.create(DABOMdataFolder)
 }
 
-
-#-----------------------------------------------------------------
-# build configuration table (requires internet connection)
-org_config = buildConfig()
-
-# customize some nodes based on DABOM framework
-configuration = org_config %>%
-  mutate(Node = ifelse(SiteID %in% c('VC2', 'VC1', 'LTR', 'MTR', 'UTR'),
-                       SiteID,
-                       Node),
-         Node = ifelse(SiteID == 'SC2',
-                       'SC2B0',
-                       Node),
-         Node = ifelse(SiteID %in% c('CROTRP',
-                                     'CRT',
-                                     'REDTRP',
-                                     'REDR',
-                                     'RRT'),
-                       'SC2A0',
-                       Node),
-         Node = ifelse(Node == 'ACB',
-                       'ACBB0',
-                       Node),
-         Node = ifelse(Node == 'CCA',
-                       'CCAB0',
-                       Node),
-         Node = ifelse(SiteID == 'AFC',
-                       ifelse(grepl('MAINSTEM', AntennaGroup),
-                              'AFCB0',
-                              'AFCA0'),
-                       Node),
-         Node = ifelse(SiteID == 'HBC',
-                       'HYCA0',
-                       Node),
-         Node = ifelse(SiteID %in% c('TUCH', 'TFH'),
-                       'TUCH',
-                       Node),
-         Node = ifelse(SiteID == 'MCCA',
-                       'STR',
-                       Node),
-         Node = ifelse(SiteID == 'CARMEC',
-                       'CRCA0',
-                       Node),
-         Node = ifelse(SiteID == 'BIG2C',
-                       'TAYA0',
-                       Node),
-         Node = ifelse(SiteID == 'WIMPYC',
-                       'WPCA0',
-                       Node),
-         Node = ifelse(SiteID == 'IML' & ConfigID == 130 & AntennaID == '09',
-                       'IMLA0',
-                       Node),
-         Node = str_replace(Node, '^BTC', 'BTL'),
-         Node = ifelse(SiteID %in% c('YANKFK', 'CEY'),
-                       'YFKA0',
-                       Node),
-         Node = ifelse(SiteID == 'SAWT',
-                       'STL',
-                       Node),
-         Node = ifelse(SiteID == 'LOOH',
-                       'LOOKGC',
-                       Node),
-         Node = ifelse(SiteID == 'RPDTRP',
-                       'RAPH',
-                       Node),
-         Node = ifelse(SiteID == 'CHARLC',
-                       'CCAB0',
-                       Node),
-         Node = ifelse(Node == 'KEN',
-                       'KENB0',
-                       Node),
-         Node = ifelse(Node == 'HYC',
-                       'HYCB0',
-                       Node),
-         Node = ifelse(Node == 'YFK',
-                       'YFKB0',
-                       Node),
-         Node = ifelse(Node == 'LLR',
-                       'LLRB0',
-                       Node),
-         Node = ifelse(Node == 'LRW',
-                       'LRWB0',
-                       Node),
-         Node = ifelse(SiteID == '18M',
-                       str_replace(Node, '18M', 'HEC'),
-                       Node)) %>%
-  distinct()
-
-
-# Node network for DABOM
-site_df = writeLGRNodeNetwork()
-
-# remove some sites that have been combined with others (see the modifications to the configuration file)
-site_df = site_df %>%
-  filter(!SiteID %in% c('TFH',
-                        'MCCA',
-                        'WIMPYC',
-                        'YANKFK', 'CEY',
-                        'SAWT',
-                        'LOOH',
-                        'CARMEC',
-                        'BIG2C',
-                        'RPDTRP'))
-
+# load configuration and site_df data
+load('./data/ConfigurationFiles/site_config.rda')
 
 #-----------------------------------------------------------------
 # read observations from PTAGIS and process with PITcleanr
 # set species
-spp = 'Steelhead'
+spp = 'Chinook'
 # have the processed capture history files been cleaned by the 
 # biologist and are they ready for DABOM
 bio_cleaned = TRUE
